ec7c635e39127447ae8a8fd8ac9c9ac467069646
Mattia Roccoberton <mroccoberton@mbpdev004.local>
2017-01-22 21:09:51 +0100

    Improved examples

--- examples/gitbooks-crystal-patterns.sh
+++ examples/gitbooks-crystal-patterns.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 URL="https://veelenga.gitbooks.io/crystal-patterns/content/"
+#CUSTOM="examples/custom.css"
+CUSTOM="examples/highlightjs-github.css"
 
-crystal src/manual-generator.cr -- -V $URL -c ".book-body .page-inner" -t ".book-summary .chapter a" -s "examples/custom.css" -o "examples/gitbooks-crystal-patterns.pdf"
+crystal src/manual-generator.cr -- -V $URL -c ".book-body .page-inner" -t ".book-summary .chapter a" -s $CUSTOM -o "examples/gitbooks-crystal-patterns.pdf"
